0e7b361ff5475c4d3262c843c30deda14a989df4
Enej Ravbar <Enej Ravbar>
2018-01-09 00:16:04 +0100

    index.html, login.html, register.html VueJS rendering dodan

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,53 @@
 $(document).ready(function(){
-  var glava = new Vue({
-    el: '#glava',
+
+  var app = new Vue({
+    el: '#app',
     data: {
+      tabelaKategorij:[
+        {ime:"Računalništvo", url:"product.html"},
+        {ime:"Bela Tehnika", url:"product.html"},
+        {ime:"Vrtnarstvo", url:"product.html"},
+        {ime:"Obleke", url:"product.html"}
+      ],
+      artikel:{
+        slika_url:"images/ba.jpg",
+        ime_artikla:"Usnjena torba",
+        redna_cena:"400",
+        znizana_cena:"300"
+      },
+      mnoziceArtiklov:{
+        slideShow:[
+              {slika_url:"images/bag.jpg", ime_artikla:"Usnjena torba", redna_cena:"400", znizana_cena:"300"},
+              {slika_url:"images/bag.jpg", ime_artikla:"Usnjena torba", redna_cena:"400", znizana_cena:"300"},
+              {slika_url:"images/bag.jpg", ime_artikla:"Usnjena torba", redna_cena:"400", znizana_cena:"300"},
+            ],
+        zadnjiArtikli:[
+              {slika_url:"images/ba.jpg", ime_artikla:"Usnjena torba", redna_cena:"400", znizana_cena:"300"},
+              {slika_url:"images/ba.jpg", ime_artikla:"Usnjena torba", redna_cena:"400", znizana_cena:"300"},
+              {slika_url:"images/ba.jpg", ime_artikla:"Usnjena torba", redna_cena:"400", znizana_cena:"300"},
+            ],
+
+      },
     }
   });
-  var noga = new Vue({
-    el: '#noga',
-    data: {
-    }
-  });
+
+  $('.example1').wmuSlider();
+});
+
+$(function() {
+    var menu_ul = $('.menu > li > ul'),
+           menu_a  = $('.menu > li > a');
+    menu_ul.hide();
+    menu_a.click(function(e) {
+        e.preventDefault();
+        if(!$(this).hasClass('active')) {
+            menu_a.removeClass('active');
+            menu_ul.filter(':visible').slideUp('normal');
+            $(this).addClass('active').next().stop(true,true).slideDown('normal');
+        } else {
+            $(this).removeClass('active');
+            $(this).next().stop(true,true).slideUp('normal');
+        }
+    });
 
 });
--- public/js/vue_components.js
+++ public/js/vue_components.js
@@ -1,5 +1,6 @@
 Vue.component('glava', {
-  template:`<div class="header">
+  template:`
+  <div class="header">
    <div class="top-header">
       <div class="container" style="height:15px;">
       </div>
@@ -18,12 +19,14 @@ Vue.component('glava', {
          </div>
          <div class="header-bottom-right">
             <div class="account" style="dislay:none"><a href="login.html"><span> </span>VAŠ RAČUN</a></div>
+
             <ul class="login">
                <li><a href="login.html"><span> </span>PRIJAVA</a></li>
                |
                <li><a href="register.html">REGISTRACIJA</a></li>
             </ul>
             <div class="cart"><a href="#"><span> </span>KOŠARICA</a></div>
+
             <div class="clearfix"> </div>
          </div>
          <div class="clearfix"> </div>
@@ -33,7 +36,8 @@ Vue.component('glava', {
 });
 
 Vue.component('noga', {
-  template:`<div class="footer">
+  template:`
+  <div class="footer">
    <div class="footer-top">
       <div class="container">
          <div class="latter">
@@ -65,4 +69,122 @@ Vue.component('noga', {
       </div>
    </div>
 </div>`
+});
+
+Vue.component('slide', {
+  props:['slika'],
+  template:`
+  <article style="position: absolute; width: 100%; opacity: 0;">
+     <div class="banner-matter">
+        <div class="col-md-5 banner-bag">
+           <img class="img-responsive " :src="slika" alt=" " />
+        </div>
+        <div class="col-md-7 banner-off">
+          <h2>FLAT 50% 0FF</h2>
+          <label>FOR ALL PURCHASE <b>VALUE</b></label>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et </p>
+          <span class="on-get">KUPI SEDAJ</span>
+        </div>
+        <div class="clearfix"> </div>
+     </div>
+  </article>`
+});
+
+Vue.component('artikel-domaca-stran', {
+  props:['slika_url','ime_artikla','redna_cena','znizana_cena','st_slike'],
+  template:`
+  <div v-if="st_slike<=1" class="col-md-4 chain-grid ">
+     <a href="single.html"><img class="img-responsive chain" :src="slika_url" alt=" " /></a>
+     <span class="star"> </span>
+     <div class="grid-chain-bottom">
+        <h6><a href="single.html">{{ime_artikla}}</a></h6>
+        <div class="star-price">
+           <div class="dolor-grid">
+              <span class="actual">{{znizana_cena}}$</span>
+              <span class="reducedfrom">{{redna_cena}}</span>
+              <span class="rating">
+              <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
+              <label for="rating-input-1-5" class="rating-star1"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
+              <label for="rating-input-1-4" class="rating-star1"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
+              <label for="rating-input-1-3" class="rating-star"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
+              <label for="rating-input-1-2" class="rating-star"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
+              <label for="rating-input-1-1" class="rating-star"> </label>
+              </span>
+           </div>
+           <a class="now-get get-cart" href="#">ADD TO CART</a>
+           <div class="clearfix"> </div>
+        </div>
+     </div>
+  </div>
+
+  <div v-else class="col-md-4 chain-grid grid-top-chain">
+     <a href="single.html"><img class="img-responsive chain" :src="slika_url" alt=" " /></a>
+     <span class="star"> </span>
+     <div class="grid-chain-bottom">
+        <h6><a href="single.html">{{ime_artikla}}</a></h6>
+        <div class="star-price">
+           <div class="dolor-grid">
+              <span class="actual">{{znizana_cena}}$</span>
+              <span class="reducedfrom">{{redna_cena}}</span>
+              <span class="rating">
+              <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
+              <label for="rating-input-1-5" class="rating-star1"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
+              <label for="rating-input-1-4" class="rating-star1"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
+              <label for="rating-input-1-3" class="rating-star"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
+              <label for="rating-input-1-2" class="rating-star"> </label>
+              <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
+              <label for="rating-input-1-1" class="rating-star"> </label>
+              </span>
+           </div>
+           <a class="now-get get-cart" href="#">ADD TO CART</a>
+           <div class="clearfix"> </div>
+        </div>
+     </div>
+  </div>
+  `
+});
+
+Vue.component('artikel-posebna-ponudba', {
+  props:['slika_url','ime_artikla','redna_cena','znizana_cena'],
+  template:`
+  <div class=" chain-grid menu-chain">
+    <a href="single.html"><img class="img-responsive chain" :src="slika_url" alt=" " /></a>
+    <div class="grid-chain-bottom chain-watch">
+       <span class="actual dolor-left-grid">{{znizana_cena}}€</span>
+       <span class="reducedfrom">{{redna_cena}}€</span>
+       <h6><a href="single.html">{{ime_artikla}}</a></h6>
+    </div>
+  </div>`
+});
+
+Vue.component('kategorija', {
+  props:['url','ime_kategorije'],
+  template:`<li><a :href="url">{{ime_kategorije}}</a></li>`
+});
+
+Vue.component('navigacijski-menu', {
+  props:[],
+  template:`
+  <div class="sub-cate">
+    <div class=" top-nav rsidebar span_1_of_left">
+       <h3 class="cate">KATEGORIJE</h3>
+       <ul class="menu">
+          <ul class="kid-menu " >
+            <slot name="kategorija" >
+            </slot>
+          </ul>
+       </ul>
+    </div>
+    <slot name="artikel-posebna-ponudba">
+    </slot>
+    <slot></slot>
+  </div>
+  `
 });
